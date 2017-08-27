524649b84d9d0b13f19bd564ccea98808ff3b49f
Hill <hill@kimo.com>
2017-08-27 08:27:30 +0000

    improve get logic

--- get.php
+++ get.php
@@ -13,14 +13,13 @@ class get extends PlugIn
     public function get($k)
     {
         $option =& \PMVC\getOption($k);
-        if (!is_null($option)) {
-            return $option;
-        } else {
+        if (is_null($option)) {
             if (is_array($this['order'])) {
                 foreach ($this['order'] as $get) {
                     $plug = \PMVC\plug($get);
                     if ($plug && $plug->has($k)) {
-                        return $plug->get($k);
+                        $option = $plug->get($k);
+                        \PMVC\option('set', $k, $option);
                     }
                 }
             }
