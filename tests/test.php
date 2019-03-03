873119a39e26ccfeac5eb384846ac6fe9be5822c
Hill <hill@kimo.com>
2019-03-03 14:33:46 +0000

    fixed unit test

--- tests/test.php
+++ tests/test.php
@@ -1,13 +1,16 @@
 <?php
-PMVC\Load::plug();
-PMVC\addPlugInFolders(['../']);
+
+namespace PMVC\PlugIn\get;
+
+use PHPUnit_Framework_TestCase;
+
 class GetTest extends PHPUnit_Framework_TestCase
 {
     private $_plug='get';
     function testPlugin()
     {
         ob_start();
-        print_r(PMVC\plug($this->_plug, ['order'=>[]]));
+        print_r(\PMVC\plug($this->_plug, ['order'=>[]]));
         $output = ob_get_contents();
         ob_end_clean();
         $this->assertContains($this->_plug,$output);
